741ef62fb8bfd36dbde38281ff9de18f90c9c623
Oleg <bender987@bk.ru>
2023-05-27 02:13:10 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SearchPhoto.Models;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SearchPhoto.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private const string ACCESS_KEY = "SJLcb8xTAsDOUNUSTHkGBHCMPYLKQofVz0sQaGpcesI";
+
+        private const string BASE_LINK = "https://api.unsplash.com/";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> SearchPhotos(string query, int page = 1)
+        {
+            var photos = await GetSearchPhotos(query, page);
+            photos.TotalPages = photos.Total / 30 + 1;
+            return Json(photos);
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> RandomPhotos()
+        {
+            var photos = await GetRandomPhotos();
+            return Json(photos);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        private async Task<PhotoData[]> GetRandomPhotos()
+        {
+            string photosData = "";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(BASE_LINK + "photos/random/?client_id=" + ACCESS_KEY + "&count=30");
+            HttpContent content = response.Content;
+            if (response.IsSuccessStatusCode)
+            {
+                photosData = await response.Content.ReadAsStringAsync();
+            }
+
+            return JsonConvert.DeserializeObject<PhotoData[]>(photosData);
+        }
+
+        [HttpGet]
+        private async Task<SearchPhotoData> GetSearchPhotos(string query, int page = 1)
+        {
+            string photosData = "";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(BASE_LINK + "search/photos/?page=" + page + "&per_page=30&client_id=" + ACCESS_KEY + "&query=" + query);
+            HttpContent content = response.Content;
+            if (response.IsSuccessStatusCode)
+            {
+                photosData = await response.Content.ReadAsStringAsync();
+            }
+
+            Debug.WriteLine(photosData);
+
+            return JsonConvert.DeserializeObject<SearchPhotoData>(photosData);
+        }
+    }
+}
---
+++ Models/PhotoData.cs
@@ -0,0 +1,150 @@
+﻿namespace SearchPhoto.Models
+{
+    public class SearchPhotoData
+    {
+        public long Total { get; set; }
+        public long TotalPages { get; set; }
+        public PhotoData[] Results { get; set; }
+    }
+
+    public class PhotoData
+    {
+        public string Id { get; set; }
+        public string Slug { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public DateTimeOffset? PromotedAt { get; set; }
+        public long Width { get; set; }
+        public long Height { get; set; }
+        public string Color { get; set; }
+        public string BlurHash { get; set; }
+        public string Description { get; set; }
+        public string AltDescription { get; set; }
+        public Urls Urls { get; set; }
+        public Links Links { get; set; }
+        public long Likes { get; set; }
+        public bool LikedByUser { get; set; }
+        public object[] CurrentUserCollections { get; set; }
+        public Sponsorship Sponsorship { get; set; }
+        public TopicSubmissions TopicSubmissions { get; set; }
+        public User User { get; set; }
+        public Tag[]? Tags { get; set; }
+    }
+
+    public class Links
+    {
+        public Uri Self { get; set; }
+        public Uri Html { get; set; }
+        public Uri Download { get; set; }
+        public Uri DownloadLocation { get; set; }
+    }
+
+    public partial class Source
+    {
+        public Ancestry Ancestry { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public string MetaTitle { get; set; }
+        public string MetaDescription { get; set; }
+        public PhotoData PhotoData { get; set; }
+    }
+
+    public partial class Tag
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public Source Source { get; set; }
+    }
+
+    public class Sponsorship
+    {
+        public string[] ImpressionUrls { get; set; }
+        public string Tagline { get; set; }
+        public Uri TaglineUrl { get; set; }
+        public User Sponsor { get; set; }
+    }
+
+    public partial class Ancestry
+    {
+        public Category Type { get; set; }
+        public Category Category { get; set; }
+        public Category Subcategory { get; set; }
+    }
+
+    public partial class Category
+    {
+        public string Slug { get; set; }
+        public string PrettySlug { get; set; }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TwitterUsername { get; set; }
+        public Uri PortfolioUrl { get; set; }
+        public string Bio { get; set; }
+        public string Location { get; set; }
+        public UserLinks Links { get; set; }
+        public ProfileImage ProfileImage { get; set; }
+        public string InstagramUsername { get; set; }
+        public long TotalCollections { get; set; }
+        public long TotalLikes { get; set; }
+        public long TotalPhotos { get; set; }
+        public bool AcceptedTos { get; set; }
+        public bool ForHire { get; set; }
+        public Social Social { get; set; }
+    }
+
+    public class UserLinks
+    {
+        public Uri Self { get; set; }
+        public Uri Html { get; set; }
+        public Uri Photos { get; set; }
+        public Uri Likes { get; set; }
+        public Uri Portfolio { get; set; }
+        public Uri Following { get; set; }
+        public Uri Followers { get; set; }
+    }
+
+    public class ProfileImage
+    {
+        public Uri Small { get; set; }
+        public Uri Medium { get; set; }
+        public Uri Large { get; set; }
+    }
+
+    public class Social
+    {
+        public string InstagramUsername { get; set; }
+        public Uri PortfolioUrl { get; set; }
+        public string TwitterUsername { get; set; }
+        public object PaypalEmail { get; set; }
+    }
+
+    public class TopicSubmissions
+    {
+        public BusinessWork BusinessWork { get; set; }
+    }
+
+    public class BusinessWork
+    {
+        public string Status { get; set; }
+        public DateTimeOffset ApprovedOn { get; set; }
+    }
+
+    public class Urls
+    {
+        public Uri Raw { get; set; }
+        public Uri Full { get; set; }
+        public Uri Regular { get; set; }
+        public Uri Small { get; set; }
+        public Uri Thumb { get; set; }
+        public Uri SmallS3 { get; set; }
+    }
+}
